ce74c74134bb1223dbeed5aa863bd0aae3d4bf19
digogc <digo.gcardoso@gmail.com>
2024-11-07 23:00:03 -0300

    implementando escrita em arquivo

--- testando_escrita_em_arquivo.asm
+++ testando_escrita_em_arquivo.asm
@@ -3,26 +3,97 @@ vetor: .double 12345.678 , 23456.789 , 34567.891 , 45678.910 , 56789.101 , 67891
 numero_10: .double 10.0
 ytest: .asciiz "C:/Users/digog/OneDrive/Área de Trabalho/Ytest.txt"
 buffer_escrita: .word 0
+linhas_matriz: .word 6
+numero_10: .double 10.0
 
 .text
+
+# Função escrever_ytest
+# Função que recebe o endereço do vetor que armazena o ytest e escreve ele em um arquivo
+# Parâmetros: endereço do vetor ytest em a0
+escrever_ytest:
+	# Salvar endereço base do ytest em s0
+	move $s0, $a0
+	
+	# Carrega a constante 10 no registrador f10
+	la $t0, numero_10
+	l.d $f10, 0($t0)
+	
+	# Como haverá jal dentro dessa função, salvar $ra na pilha
+	subi $sp, $sp, 4
+	sw $ra, 0($sp)
+	
 	# Abrir o arquivo
 	li $v0, 13
 	la $a0, ytest
 	li $a1, 9
 	syscall # Descritor do arquivo está em v0
 	
-	move $s0, $v0	# Salvo o descritor do arquivo
-
-	li $a0, 7
-	jal escrever_ascii
+	move $s1, $v0 	# Descritor do arquivo está em s1
 	
+	li $s2, 0 	# Contador de linhas escritas sem s2
+	# Loop para acessar todas as linhas
+	acessar_linha:
+		beq $s2, $s3, fim_acessar_linhas
+		# Inicializa N para quantidade de divisões por 10
+		li $s4, 0
+		# Encontrar endereço da linha
+		mul $t0, $s2, 8
+		add $t0, $t0, $s0
+		# Carrega o valor que esta na linha em f0
+		l.d $f0, 0($t0)
+		# Começa calculo para encontrar quantas divisões devem ser feitas
+		calcular_N:
+			c.lt.d $f0, $f10		# Se o numero da linha for menor que 10
+			bc1t fim_calcular_N	# Já posso começar o processo de escrita
+			
+			div.d $f0, $f0, $f10 	# Divido o valor por 10
+			addi $s4, $s4, 1		# N = N + 1
+			j calcular_N
+		
+		fim_calcular_N:
+		li $s5, 0	# Inicializo o contador de multiplicações ja realizadas
+		# Agora preciso salvar o primeiro digito do double para escrever no arquivo
+		escreve_antes_do_ponto:
+			beq $s5, $s4, fim_escreve_antes_do_ponto
+			# tranformação do primeiro digito do double em int
+			cvt.w.d $f2, $f0		# Salva a parte inteira do valor em um registrador de double
+			mfc1 $s6, $f2		# Move a parte inteira do valor para um registrador int
+			move $a0, $s6		# Passo a parte inteira como parametro
+			jal escrever_ascii
+			
+			# Subtrai a parte inteira e multiplica por 10
+			
+			
+			#incrementa o contador de multiplicações
+			addi $s5, $s5, 1
+			j escreve_antes_do_ponto
+
+			
+			
+			
+			
+			
+			
+			
+			
+			fim_escreve_antes_do_ponto
+		
+		
+		
+		
+		
+		
+		j acessar_linha
+	fim_acessar_linhas:
+	# Restaura para o $ra da função chamadora
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
 	# Fechar o arquivo
 	li $v0, 16
-	move $a0, $s0
+	move $a0, $s0	# passar como argumento o descritor
 	syscall
 	
-	li $v0, 10
-	syscall
 	
 ############################## FUNÇÃO QUE CALCULA O ASCII PAR O INTEIRO E ESCREVE NO ARQUIVO
 #Recebe: valor inteiro do numero que deve ser escrito em a0
